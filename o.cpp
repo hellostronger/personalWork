9e72627cc033d98c80b464b9a0e726c344403dc2
hellostronger <48884346+hellostronger@users.noreply.github.com>
2020-03-14 00:39:20 +0800

    Update o.cpp

--- o.cpp
+++ o.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 int getnum_char(char f[]);
 int getnum_row(char f[]);
+int getnum_word(char f[]);
+int getnum_zhushirow(char f[]);
 int main(int i,char *argv[]){
 	printf("%d",i);
 	if(i!=3)
@@ -19,6 +21,9 @@ int main(int i,char *argv[]){
 	printf("%s文件中的字符数为%d\n",argv[1], g);	
 	} 
 	printf("%s文件中的行数为%d",argv[1],  getnum_row(argv[2])+1);
+	printf("\n");
+	printf("单词数为%d\n",getnum_word(argv[2])); 
+	
 	return 0;
 	
 	}
@@ -50,14 +55,96 @@ int getnum_row(char f[]){
 
 int getnum_word(char f[]){
 		FILE *fp;
+		bool flag = true;
 		int  ch;
 		int num = 0;
 		fp = fopen(f,"r");
 		while((ch=getc(fp))!=EOF){
-			if((char)ch=='\n')
-			num++;
-			printf("%c",ch);
+			if(ch>='a'&&ch<='z'||ch<='Z'&&ch>='A'){
+				if(flag==true){
+					num++;
+					flag = false;
+				}	
+			}else{
+					flag = true;
+				}
 		}
         return num;
-        666 
 } 
+
+int getnum_zhushirow(char f[]){
+	  FILE *fp;
+	  bool flag = true;
+		int  ch;
+		int num = 0;
+		fp = fopen(f,"r");
+		while((ch=getc(fp))!=EOF){
+			if(ch>='a'&&ch<='z'||ch<='Z'&&ch>='A'){
+				if(flag==true){
+					num++;
+					flag = false;
+				}	
+			}else{
+					flag = true;
+				}
+		}
+        return num;
+    }
+    
+    
+int getnum_zhushirow(char f[]){
+	bool tag = true;
+		bool j = false;
+		bool end_tag = false;
+		FILE *fp;
+		int  ch;
+		bool flag = false;
+		bool duanzhushi = false; 
+		bool hangzhushi = false;
+		int num = 0;
+		fp = fopen(f,"r");
+		while((ch=getc(fp))!=EOF){
+			if(ch=='/'&&(!duanzhushi||!hangzhushi)){
+				flag = true;//读入第一个/ 		 
+		}
+		if(flag&&j){  //读入第二行 
+					if(ch == '*'){ //多段注释
+						duanzhushi = true;
+					}if(ch == '/'){//单行注释 
+						hangzhushi = true;
+					}
+					
+				}
+		
+		if(duanzhushi){
+			if(ch =='\n'){
+			num++;	
+			printf("%d\n",num);
+			}
+		}
+		if(hangzhushi){
+			if(ch =='\n'){
+				num++;
+				hangzhushi=false;
+				flag = false;
+				j = false;	
+			}
+			printf("%d666\n",num);
+				}
+		if(ch == '*'&&duanzhushi){
+			end_tag = true;
+		}
+		if(ch == '/'&&end_tag){
+			duanzhushi = false;
+			end_tag = false;
+			flag = false;
+			j = false; 
+		}	
+		if (flag == true)
+		j = true;	
+	}
+	printf("文件含%d行注释",num); 
+}
+	
+
+
